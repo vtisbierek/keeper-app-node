c7b604e235155cd5d5ae3360830b17ed9a007077
Victor Tisbierek <metropoller@gmail.com>
2023-02-26 08:23:08 -0300

    Adding Google Login w/ OAuth2.0

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const fetch = require("node-fetch");
+const { google } = require("googleapis");
 
 const app = express();
 
@@ -23,6 +26,68 @@ const noteSchema = new mongoose.Schema(
 
 const Note = new mongoose.model("Note", noteSchema);
 
+const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    "http://localhost:8000/handleGoogleRedirect" // server redirect url handler
+);
+
+app.get("/handleGoogleRedirect", async (req, res) => {
+    // get code from url
+    const code = req.query.code;
+    console.log("server 36 | code", code);
+    // get access token
+    oauth2Client.getToken(code, (err, tokens) => {
+      if (err) {
+        console.log("server 39 | error", err);
+        throw new Error("Issue with Login", err.message);
+      }
+      const accessToken = tokens.access_token;
+      const refreshToken = tokens.refresh_token;
+  
+      res.redirect(
+        `http://localhost:3000?accessToken=${accessToken}&refreshToken=${refreshToken}`
+      );
+    });
+});
+
+app.post("/getValidToken", async (req, res) => {
+    try {
+      const request = await fetch("https://www.googleapis.com/oauth2/v4/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          client_id: process.env.GOOGLE_CLIENT_ID,
+          client_secret: process.env.GOOGLE_CLIENT_SECRET,
+          refresh_token: req.body.refreshToken,
+          grant_type: "refresh_token",
+        }),
+      });
+  
+      const data = await request.json();
+      console.log("server 74 | data", data.access_token);
+  
+      res.json({
+        accessToken: data.access_token,
+      });
+    } catch (error) {
+      res.json({ error: error.message });
+    }
+});
+
+app.post("/createAuthLink", cors(), (req, res) => {
+    const url = oauth2Client.generateAuthUrl({
+      access_type: "offline",
+      scope: [
+        "https://www.googleapis.com/auth/userinfo.email",
+        "https://www.googleapis.com/auth/userinfo.profile"
+      ],
+      prompt: "consent",
+    });
+    res.send({ url });
+});
 
 app.route("/")
     .get((req, res) => {
